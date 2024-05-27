6becf2fa75e36412bb99cccd04cccdb4cfcef540
safreya <safreya@qq.com>
2024-05-27 14:38:13 +0800

    修正翻译，增加css设定

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf zsh_html
 cp ../zsh.zh.texi zsh.texi
 sed  -i '' -e "s/@c %\*\*end of header/@c %\*\*end of header\n\n@ifhtml\n@set dsq ''\n@set dsbq \`\`\n@end ifhtml/"  zsh.texi
 # makeinfo --force --error-limit=999999 --split=chapter --html zsh.texi
-makeinfo --force --error-limit=999999 --no-split --no-headers --html zsh.texi
+makeinfo --force --error-limit=999999 --no-split --no-headers --html --css-include=../zshhtml.css zsh.texi
 
 for file in ../comments/*.md; do
 	if [ -f "$file" ]; then
